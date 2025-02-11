d4162df7db93fdeb66e113731367fd655fd77fb2
Arblind13 <arblind.elshani@student.uni-pr.edu>
2025-02-12 00:08:48 +0100

    Funksioni Void

--- Grupi 34.cpp
+++ Grupi 34.cpp
@@ -15,3 +15,20 @@ struct Makina {
     int viti;
     LlojiMakines lloji;
 };
+// Funksion per te printuar informacionin e makines void printoMakinen (const Makina) {cout << "Marka: " << makina.marka <<endl; cout"Modeli: " << mode;}
+ void printoMakinen(const Makina makina) {
+    cout << "Marka: " << makina.marka << endl;
+    cout << "Modeli: " << makina.modeli << endl;
+    cout << "Viti: " << makina.viti << endl;
+    cout << "Lloji: ";
+    switch (makina.lloji) {
+        case LlojiMakines::Sedan:
+            cout << "Sedan" << endl;
+            break;
+        case LlojiMakines::SUV:
+            cout << "SUV" << endl;
+            break;
+        case LlojiMakines::Kamion:
+            cout << "Kamion" << endl;
+            break;
+    }
