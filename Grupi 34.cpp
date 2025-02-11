a05d59b0b591509e59f252948779effedac98e7a
Ramë Hadergjonaj <ramhadergjonaj05@gmail.com>
2025-02-11 01:01:31 +0100

    Enum, struktura vetura

--- Grupi 34.cpp
+++ Grupi 34.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+// Definimi i nje enum per llojet e makinave
+enum class LlojiMakines {
+    Sedan,
+    SUV,
+    Kamion
+};
+
+// Struktura per te perfaqesuar nje makine
+struct Makina {
+    string marka;
+    string modeli;
+    int viti;
+    LlojiMakines lloji;
+};
